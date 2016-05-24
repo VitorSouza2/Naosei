a4986e874c92e1ccc01ad52ae4f8446dced59cd7
VitorSouza2 <e0404d21@mohmal.in>
2016-05-24 15:16:30 -0300

    Update eoq.cpp

--- eoq.cpp
+++ eoq.cpp
@@ -11,7 +11,7 @@ main(){
 	printf("Informe o numero 2:\n");
 	scanf("%f",n2);
 	
-	printf("Para somar os numeros digite: 1 \n Para subtrair os numeros digite: 2 \n");
+	printf("Para somar os numeros digite: 1 \n Para subtrair os numeros digite: 2 \n Para multiplicar os numeros digite: 3 \n Para fazer a divisão os numeros digite: 4 \n");
 	scanf("%i",nsei);
 	
 	if(nsei == 1){
@@ -22,4 +22,12 @@ main(){
 		resultado = n1 - n2;
 		printf("O resultado de sua subtracao eh: %f \n",resultado);
 	}
+		else if(nsei == 3){
+			resultado = n1 * n2;
+			printf("O resultado de sua multicacao eh: %f \n",resultado);
+		}
+			else if(nsei == 4){
+			resultado = n1 / n2;
+			printf("O resultado de sua divisao eh: %f \n",resultado);
+			}
 }
